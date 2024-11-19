514fac2f0102aed4605d16033dfbd79d42727fc7
jlrios <rdjl5791@gmail.com>
2024-11-19 09:49:10 -0600

    Crisium text adventure game, base project.

---
+++ crisium-game/src/Message.tsx
@@ -0,0 +1,12 @@
+function Message() {
+    // JSX: JavaScript XML.
+    return (
+        <pre>
+            <output>
+                CRISIUM
+            </output>
+        </pre>
+    );
+}
+
+export default Message;
